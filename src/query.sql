b7e2372f71cee1c641167758cf81d4154e44c0b2
Matt Holden <holden.matt@gmail.com>
2023-05-02 17:16:56 -0600

    Convert to cumulative counts

--- src/query.sql
+++ src/query.sql
@@ -1,3 +1,4 @@
+-- Get all star events
 WITH star_events AS (
   SELECT
     repo.name as repo,
@@ -9,6 +10,7 @@ WITH star_events AS (
     AND repo.name != "/"
 ),
 
+-- Filter to the top 10k repos by total stars
 top_repos AS (
   SELECT
     repo,
@@ -17,14 +19,24 @@ top_repos AS (
   GROUP BY 1
   ORDER BY 2 DESC
   LIMIT 10000
+),
+
+-- Aggregate monthly stars (by unique users)
+monthly_counts AS (
+  SELECT
+    repo,
+    format_date("%Y-%m-01", created_at) as month,
+    COUNT(DISTINCT actor_id) as monthly_count
+  FROM star_events
+  WHERE
+    repo IN (SELECT repo FROM top_repos)
+  GROUP BY 1, 2
 )
 
+-- Convert monthly star counts to cumulative totals
 SELECT
   repo,
-  format_date("%Y-%m-01", created_at) as month,
-  COUNT(DISTINCT actor_id) as monthly_stargazers
-FROM star_events
-WHERE
-  repo IN (SELECT repo FROM top_repos)
-GROUP BY 1, 2
+  month,
+  SUM(monthly_count) OVER (PARTITION BY repo ORDER BY month ROWS UNBOUNDED PRECEDING) as approx_stars
+FROM monthly_counts
 ORDER BY 2 DESC, 3 DESC
